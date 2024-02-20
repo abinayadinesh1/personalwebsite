8b5079c4fe9f2ee9a7657d5014bd3bc3849e5d0b
Abinaya Dinesh <abinayadinesh1@gmail.com>
2024-02-20 01:26:33 -0800

    read the news

--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -2,6 +2,8 @@
 <a href="./writing/economiesofscale"
   >2.17.24 why do economies at scale break down with agriculture?</a
 >
+<br />
+<a href="./writing/readthenews">12.28.23 should tech people read the news?</a>
 <!-- 
 <a href=""></a>
 <a href=""></a>
---
+++ src/routes/writing/readthenews/+page.svelte
@@ -0,0 +1,108 @@
+<h1>should tech people read the news?</h1>
+<h2>alternate title: work on what matters</h2>
+<p>
+  A lot of people, especially in the tech industry, think reading the news is a
+  waste of time. It pulls time away from shipping product, or reading OpenAI’s
+  latest blog post. Even the meditation I listen to at night warns me not to
+  read the paper before bed, beause of how emotionally draining it can be. And
+  yes, they’re all right.
+</p>
+<p>
+  But how else do you know about the humanities biggest issues, without
+  listening to the people reporting on it? It seems impossible to find PMF for a
+  problem you made up. But there are about a million problems in our world for
+  us to fix; climate change, lack of humanitarian aid, surveillance, renewable
+  energy, corruption, inequitable transportation, poverty, and chronic disease
+  (just to name a few). ^3 None of us can solve all of these problems. And some
+  of them might even be totally out of our reach, requiring cultural and
+  political movements instead. But, I can’t help but imagine how much more
+  creative and useful hackathons for social good would be if more people
+  actually knew what was happening in the world. What would happen if just 10%
+  of people currently working on side projects or startups wanted to find a way
+  to help people?
+</p>
+<p>
+  It’s a fair argument to say that “helping people doesn’t pay” or “there’s no
+  such thing as altruism”. It definitely can’t compare to the 6-7 figure salary
+  of being a ML scientist or Optimization Engineer at Amazon. But I’d have to
+  argue that there’s a lot more opportunity in it than there used to be, and
+  just as much risk when compared to starting a B2B Saas in today’s saturated
+  markets! It takes a few cold emails, finding the right people, or keeping your
+  eyes out for grants in order to get a project like this off the ground; there
+  are plenty of VC’s willing to back them too. Here’s a list^1 I conjured of a
+  few I know of: lmk if you know any too!
+</p>
+<p>Here’s just one reason that should convince everyone to read the news:</p>
+<ol>
+  <li>
+    It doesn’t take very long! Log into The New York Times once a week, give it
+    a scroll like it’s Twitter, pick the most interesting article, and skim
+    through it. At the minimum it takes 4 minutes, and you will always learn
+    something new.
+  </li>
+</ol>
+<p>Here’s two more:</p>
+<ol>
+  <li>
+    History really does repeat itself; knowing what’s going on in the world
+    helps you understand the flow of money, understand strangers, and safegaurd
+    you and the people you love from disaster.
+  </li>
+  <li>
+    It makes you a more educated voter. Every time voting season comes around,
+    I’m always surprised that it’s here and have no idea who to vote for. This
+    is the first year I’m of legal age, and instead of searching up “political
+    view of candidate x” the day before the deadline and finding trashy
+    publicity on everyone, I’d rather build an intuition for who my future
+    leaders will be.
+  </li>
+</ol>
+<p>
+  I am forever amazed by how fast technology moves. Sitting in on a computer
+  vision workshop at BAIR^2 last month, I realized how much insane, sci-fi-level
+  tech is being shipped (my favs were on 4D View Reconstruction, Long Range
+  Point Tracking, and CLIP interpretation with text). The CS community has come
+  so far: most papers now make their code open source and include github pages
+  and documentation on how to use it!! Ready to go!!! Being privileged enough to
+  live in the heart of Silicon Valley and surrounded by researchers,
+  programmers, you have the power to use this tech for good.
+</p>
+<h1 id="recommendations">Recommendations</h1>
+<p>
+  I’ve recently been blown away by The New York Time’s committment to reporting.
+  Recently, they sued the US Health and Humanitarian Services for not releasing
+  all their data on the current migration crisis from Central America. For every
+  anecdotal story they release about the Israel-Palestine conflict, they have
+  done hundreds of interviews. Guys, I interviewed 20 farmers once to try and
+  iterate on a product I was building that helps them analyze their soil. It
+  took me a week, and more than 50 cold calls. Could you even imagine 100
+  interviews for one line in a news article? NYT has to be my favorite outlet.
+</p>
+<p>
+  I also recommend The Economist for these sick monthly reviews they put out
+  about the world economy and markets. NPR is my favorite to listen to for more
+  lighthearted content, too.
+</p>
+<p>
+  ps. I’m new to following the news (nobody in my immigrant family thought it’d
+  be more important that applying for internships), so my recommendations are
+  definitely non exhaustive.
+</p>
+<p>
+  ps. If you want a free new york times subscription, im happy to send account
+  details. It’s that important.
+</p>
+<ol>
+  <li>not ready yet</li>
+  <li>
+    <a
+      href="https://docs.google.com/document/d/1u4jGE5NQX2Zg_TRQ5E2hi4Ax3LTzEcMFVfOJpTk9kX8/edit?usp=sharing"
+      >https://docs.google.com/document/d/1u4jGE5NQX2Zg_TRQ5E2hi4Ax3LTzEcMFVfOJpTk9kX8/edit?usp=sharing</a
+    >
+  </li>
+  <li>
+    found this list of climate related problems, organized by sector <a
+      href="https://drawdown.org/solutions">https://drawdown.org/solutions</a
+    >
+  </li>
+</ol>
