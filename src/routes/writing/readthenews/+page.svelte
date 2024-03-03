29720c3882ba1590956a01879a3a1608211f4434
Abinaya Dinesh <abinayadinesh1@gmail.com>
2024-03-03 12:25:15 -0800

    fixed read the news article

--- src/routes/writing/readthenews/+page.svelte
+++ src/routes/writing/readthenews/+page.svelte
@@ -1,36 +1,50 @@
 <h1>should tech people read the news?</h1>
-<h2>alternate title: work on what matters</h2>
+<h3>alternate title: work on what matters</h3>
 <p>
   A lot of people, especially in the tech industry, think reading the news is a
-  waste of time. It pulls time away from shipping product, or reading OpenAI’s
-  latest blog post. Even the meditation I listen to at night warns me not to
-  read the paper before bed, beause of how emotionally draining it can be. And
-  yes, they’re all right.
+  waste of time. It pulls time away from shipping product or reading OpenAI's
+  latest blog post. Even the meditation I listen to warns me not to read the
+  paper, beause of how emotionally draining it can be. They're all right, to
+  some degree.
 </p>
 <p>
-  But how else do you know about the humanities biggest issues, without
-  listening to the people reporting on it? It seems impossible to find PMF for a
-  problem you made up. But there are about a million problems in our world for
-  us to fix; climate change, lack of humanitarian aid, surveillance, renewable
-  energy, corruption, inequitable transportation, poverty, and chronic disease
-  (just to name a few). ^3 None of us can solve all of these problems. And some
-  of them might even be totally out of our reach, requiring cultural and
-  political movements instead. But, I can’t help but imagine how much more
-  creative and useful hackathons for social good would be if more people
-  actually knew what was happening in the world. What would happen if just 10%
-  of people currently working on side projects or startups wanted to find a way
-  to help people?
+  But how else are you to know about humanities biggest issues, without
+  listening to the people reporting on it? Plus, it seems impossible to find PMF
+  for a problem you made up. On the other hand, there are millions of problems
+  in our world to fix: climate change, lack of humanitarian aid, surveillance,
+  renewable energy, corruption, inequitable transportation, poverty, and chronic
+  disease (just to name a few). ^3
 </p>
 <p>
-  It’s a fair argument to say that “helping people doesn’t pay” or “there’s no
-  such thing as altruism”. It definitely can’t compare to the 6-7 figure salary
-  of being a ML scientist or Optimization Engineer at Amazon. But I’d have to
-  argue that there’s a lot more opportunity in it than there used to be, and
-  just as much risk when compared to starting a B2B Saas in today’s saturated
-  markets! It takes a few cold emails, finding the right people, or keeping your
-  eyes out for grants in order to get a project like this off the ground; there
-  are plenty of VC’s willing to back them too. Here’s a list^1 I conjured of a
-  few I know of: lmk if you know any too!
+  These are huge problems to take on, and some of them might need cultural and
+  political movements to truly fix. However, as Dwarkesh Patel says,
+</p>
+<blockquote>
+  <a href="https://meridian.mercury.com/dwarkesh-patel">
+    "In another sense, there’s something strange in terms of domains you think
+    of as crowded and totally competitive. There are usually totally obvious
+    blind spots."</a
+  >
+</blockquote>
+
+<p>
+  Considering the rate of progess in technology, I predict many niche problems
+  in the world actually could be solved with 2-3 years of dedicated effort. To a
+  lighter degree, could hackathons for social good be more impactful if people
+  knew what was happening outside of their own bubble? What if 10% of people
+  working on side projects or startups wanted to find a way to help people?
+</p>
+<p>
+  It’s also a fair argument to say that “helping people doesn’t pay” or “there’s
+  no such thing as altruism”. It definitely can’t compare to the 6-7 figure
+  salary of being a ML scientist or Optimization Engineer at Amazon. But I’d
+  have to argue that there’s a lot more opportunity in it than there used to be,
+  and just as much risk when compared to starting a B2B Saas in today’s
+  saturated markets! To get an ambitious project off the ground, it only takes a
+  few cold emails, the right people, or keeping your eyes out for grants. VC
+  money trickles in this space, but there are a few (LowerCarbon, Founders Fund)
+  that are willing to take on higher risks to support inventions for the future.
+  Here’s a list^1 I conjured of a few I know of: lmk if you know any too!
 </p>
 <p>Here’s just one reason that should convince everyone to read the news:</p>
 <ol>
@@ -56,37 +70,45 @@
     publicity on everyone, I’d rather build an intuition for who my future
     leaders will be.
   </li>
+  <li>
+    Using the news to educate yourself on problems happening outside of your
+    geopolitical climate might open up a world of opportunities on what to
+    build, who to serve, and what powers might be in play against it.
+  </li>
 </ol>
 <p>
-  I am forever amazed by how fast technology moves. Sitting in on a computer
-  vision workshop at BAIR^2 last month, I realized how much insane, sci-fi-level
+  I am amazed by how fast technology moves. Last month, I was sitting in on a
+  computer vision workshop at BAIR^2 and realized how much insane, sci-fi-level
   tech is being shipped (my favs were on 4D View Reconstruction, Long Range
-  Point Tracking, and CLIP interpretation with text). The CS community has come
-  so far: most papers now make their code open source and include github pages
-  and documentation on how to use it!! Ready to go!!! Being privileged enough to
-  live in the heart of Silicon Valley and surrounded by researchers,
-  programmers, you have the power to use this tech for good.
+  Point Tracking, and CLIP interpretation with text).
 </p>
-<h1 id="recommendations">Recommendations</h1>
+<p>
+  The CS community has come so far: most papers now make their code open source
+  and include github pages and documentation on how to use it!! Ready to go!!!
+  As someone privileged enough to live in the heart of Silicon Valley and
+  surrounded by researchers, programmers, and builders, you have the power to
+  use this tech for good.
+</p>
+<h2>Recommendations</h2>
 <p>
   I’ve recently been blown away by The New York Time’s committment to reporting.
   Recently, they sued the US Health and Humanitarian Services for not releasing
   all their data on the current migration crisis from Central America. For every
   anecdotal story they release about the Israel-Palestine conflict, they have
-  done hundreds of interviews. Guys, I interviewed 20 farmers once to try and
-  iterate on a product I was building that helps them analyze their soil. It
-  took me a week, and more than 50 cold calls. Could you even imagine 100
-  interviews for one line in a news article? NYT has to be my favorite outlet.
+  done hundreds of interviews. I once interviewed 20 farmers to iterate on a
+  product I was building for soil health analysis. It took me a week, and more
+  than 50 cold calls. Could you even imagine 100 interviews for one line in a
+  news article? NYT has to be my favorite outlet.
 </p>
 <p>
-  I also recommend The Economist for these sick monthly reviews they put out
-  about the world economy and markets. NPR is my favorite to listen to for more
-  lighthearted content, too.
+  I also recommend The Economist for these sick monthly reviews and short daily
+  updates they put out about the world economy and markets. NPR is great for
+  less technical, broad yet specific stories on current events.
 </p>
 <p>
-  ps. I’m new to following the news (nobody in my immigrant family thought it’d
-  be more important that applying for internships), so my recommendations are
-  definitely non exhaustive.
+  ps. I’m new to following the news (nobody in my family thought it’d be more
+  important that applying for internships), so my recommendations are non
+  exhaustive.
 </p>
 <p>
   ps. If you want a free new york times subscription, im happy to send account
