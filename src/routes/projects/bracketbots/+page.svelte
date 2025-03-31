cefd6bc5832aadd327334d37929776a9ec12ffca
Abinaya Dinesh <adinesh@berkeley.edu>
2025-03-31 17:12:32 +0900

    fixed a little bit of the writing page

---
+++ src/routes/projects/bracketbots/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+import SvelteMarkdown from "svelte-markdown";
+const source = `
+### a friendly 5 foot tall bot that can make sandwiches for you
+
+over the weekend, i flew to across the country for the BEST hardware hackathon to take place, hosted by Brian Machado at UWaterloo.  
+
+in < 14 hours of pure engineering, we built a 'wifey' bot that can make sandwiches for you!!
+
+`
+
+</script>
--- src/routes/reading/+page.svelte
+++ src/routes/reading/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+  
+</script>
+
 <h1>Reading</h1>
 
 <h3>Books</h3>
@@ -5,11 +9,24 @@
     <li>
       <p>02.04.25 The Burden of Joy, by Lexi Kent-Monning</p>
     </li>
+    <li>
+      <p>01.30.25 The Idea Factory, by Jon Gertner</p>
+    </li>
+    <li>
+      <p>01.16.25 15 Myths on Homelessness, by Mary Brosnahan</p>
+    </li>
   </ul>
 
 <h3>Textbooks</h3>
 <p>Coming soon ~</p>
-<!-- do a grid ! -->
+<div class="image-column">
+  <div class="image-grid">
+    <img src={img1} alt="an image of abi" />
+    <img src={img2} alt="an image of abi" />
+    <img src={img3} alt="an image of abi" />
+    <img src={img4} alt="an image of abi" />
+  </div>
+</div>
 <h3>Courses</h3>
 
 <ul>
@@ -17,3 +34,6 @@
     <a href = "./reading/underactuated_robotics">Underactuated Robotics</a>
   </li>
 </ul>
+
+
+
