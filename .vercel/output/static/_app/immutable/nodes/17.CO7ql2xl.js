be0410850f5c2dfc4f749166c787d518db401e6b
Abinaya Dinesh <abinayadinesh1@gmail.com>
2025-12-08 11:54:28 -0500

    adding seneca stuff

---
+++ .vercel/output/static/_app/immutable/nodes/17.CO7ql2xl.js
@@ -0,0 +1,78 @@
+import{s as _e,e as s,a as r,c as a,g as o,b as l,j as He,i as n,n as ee,d as i}from"../chunks/scheduler.Cu-e96pa.js";import{S as Le,i as Me}from"../chunks/index.DvinRJN7.js";function Pe(Te){let h,te="Park Farming",A,m,ne="Not a farm, a decades long experiment with regenerative farming.",j,d,ie=`This was one of the most transformational calls I've had, with such an
+  educated, open minded farmer. I'd like to tell a story, not describe an
+  interview, but don't think I can paint his picture in well enough detail.
+  I'll leave it up to your own interpretation and use A to describe the
+  farmer I was talking to, to protect his identity.`,S,u,se="Surprises to me:",W,p,ae=`<li>where A lives, there are only 8 houses in 10 miles. he can go 2-3 weeks
+    without seeing a stoplight. He rarely gets to deal with other farmers, but
+    actively seeks partnerships with professors and corporations.</li> <li>A has kept a journal, which, for over the past 50 years, has recorded every
+    day of farming along with changes he made that helped/hurted the farm. This
+    could be recorded as an IMMENSE long term study, but as simple personal
+    notes, they are really messy and don&#39;t contain much qualitative data.</li> <li>A&#39;s farm has a symbiotic relationship with a company that managees
+    livestock: he lets about 4-5000 sheep graze cover crops on their farm, which
+    naturally cuts them down when it comes to growing season and feeds the sheep
+    with yummy grasses for half the year. It&#39;s a compleetely symbiotic
+    relationship, with neither side charging the other. Such an awesome example
+    of holistic management.</li>`,z,f,oe=`<strong>What is the main problem you see that prevents conventional farmers from
+    going regenerative? What are the biggest hurdles?</strong><br/>The problem is the transition can&#39;t be done in piecemeal. It could
+  have huge positive impacts for the environment, local community, and farmer.
+  But, it&#39;s very complex because there are so many forces with vested
+  interest that dont want change to occur. It can be possible to impact one
+  (sector), but not another.`,U,v,re=`Universities, state, corportions, AND the CONSUMER (consumer hesitates to pay
+  any more for better food) are all huge players. With relevance to a farmer,
+  the main issue is "who's gonna pay the difference when the yields are
+  less or the farmer can't grow as much as he transitions?".`,N,y,le=`What farmers really want is better INSURANCE. They want an umbrella to protect
+  them, giving them the space to venture out and experience different ways to
+  farm. Moreover, practicing regenerative agriculture isn't just having
+  cover crops. Spraying pesticides all over your soil even once, will
+  instantaneously revert all progress you made in cultivating soil
+  microbacteria. So, it's got to be all or nothing.`,E,c,he=`There isn't a lot of data to support regen ag. Professors will kill (him)
+  - but too much of research is deductive reasoning: asking questions like,
+  "what is the impact of nitrogen on soil?". It's too piecemeal.`,O,g,ue=`On A farm, he considers everything. Their borders, their timing, their
+  planning. Sometimes stuff he does has an impact 6 years later, which is hard
+  to measure. He's curated a system that reduces inputs and lets his system
+  develop naturally, using long term thinking thats impossible for farmers that
+  are optimizing for crop yield, harvest by harvest.`,B,w,me=`A major problem of our system is that everything instant gratification.
+  Landowners want as much money as they can every acre every year. If farmers
+  aren't yielding high, the lease owners will give the land to someone else.`,D,b,de=`<strong>What is your background? Did you have any formal training in agriculture?</strong>
+  This is his 50th year farming. He switched to regen ag in 1985 when he saw the
+  ground was getting worse and worse. He began with just putting wheatstraw into
+  the soil and was 100% serendiputous. A says he learns more from mistakes than victories,
+  and it can be really hard to draw conclusions in farming: &quot;what was the factor
+  that made my crop do X that year?&quot; His farms always has trials going, doing
+  comparative analyses with one type of practice vs another. They almost went bankrupt,
+  twice.`,F,x,pe=`He plays with the ideas of minimalism vs manipulation, which is what most
+  farmers do. They sterilize the whole environment and create what you want
+  through inputs, with no trust in the natural system.`,R,C,fe=`The best way to start the transition to regeneration is to get away from
+  sterile environment. Try creating live roots, and add biomass. The soil and
+  ground should be spongy. You don't need all the nitrogen fertilizers you
+  think.`,Q,k,ve=`He started regen farming 37 years ago, and had done everything with no support
+  from the state or university. With decades of initiative, he made
+  relationships with professors from UCDavis, UC Berkeley, and UC Extension
+  schools to share his knowledge and progress the field of regen ag.`,X,T,ye=`<strong>What is the main motivator for a farmer to start the change, from
+    conventional to regen?</strong>`,Y,_,ce=`<li>Farmers need to be convinced, with all 6 of their senses, what regen ag will
+    do for them. If they can see things, walk on the ground, smell the ground
+    and put their shovels in to see the soil, they can start to understand.</li>`,G,H,ge=`<em>a huge surprise to me:</em> some conventional farmers dont even see worms in
+  their soils. they dont remember, know, or feel what it means to have healthy, microbial
+  enriched soils.`,J,L,we=`<li>farmers need to see numbers. research needs to be done that quantifies just <strong>how much</strong>
+    less water, fertilizer, time, and inputs are being used. how tillage times are
+    shortening and tractors run faster.
+    <strong>on its own, regenerative farming doesnt increase yield, but it reduces
+      the bottom line of money spent to run the farm.</strong> who cares how you make your dollar?</li>`,K,M,be=`farmers need to see hard numbers from someone they trust. It can take 3-4
+  years for the ground to change, so they cant depend on the instantaneous
+  result anymore.`,V,P,xe=`<strong>how bad is tilling for our soils? if so, why does it have such an extensive
+    history, indicating its benefit for soil?</strong>
+  With some organic/regen farms, people are throwing it out. If you only maintain
+  1-acre, you can pull out weeds yourself or get volunteers to help. But it really
+  matters at scale: no-till on any size is almost nonexistent.`,Z,I,Ce=`On his farm, he uses targetting tilling: only killing what you need to and
+  till everything shallow (2-3 inches). A only uses tilling to a) kill weeds or
+  b) make a seedbed. He doesn't depend on it, like other farmers do, for
+  fertility and yields. His tractors run on the same path for 12 years, reducing
+  the amount of ground getting disturbed. o make this work, he builds soil
+  structure with other methods: putting compost out, biomass, and tending to the
+  roots. True, weeds reduce naturally over time. Their older organic fields are
+  their cleanest ones. But this isn't bulletproof; forget about insects,
+  disease, and fertility: A's biggest spend is on weeds, and is the only
+  thing holding them back from going no-till.`,$,q,ke=`<strong>*Quick reflection</strong>: These write-ups are to make my
+  conversations with farmers more accessible and shareable. The writing is
+  unpolished, but it allows me to work faster, so I hope the reader doesn&#39;t
+  mind !* A is bestie.`;return{c(){h=s("h1"),h.textContent=te,A=r(),m=s("p"),m.textContent=ne,j=r(),d=s("p"),d.textContent=ie,S=r(),u=s("h2"),u.textContent=se,W=r(),p=s("ul"),p.innerHTML=ae,z=r(),f=s("p"),f.innerHTML=oe,U=r(),v=s("p"),v.textContent=re,N=r(),y=s("p"),y.textContent=le,E=r(),c=s("p"),c.textContent=he,O=r(),g=s("p"),g.textContent=ue,B=r(),w=s("p"),w.textContent=me,D=r(),b=s("p"),b.innerHTML=de,F=r(),x=s("p"),x.textContent=pe,R=r(),C=s("p"),C.textContent=fe,Q=r(),k=s("p"),k.textContent=ve,X=r(),T=s("p"),T.innerHTML=ye,Y=r(),_=s("ol"),_.innerHTML=ce,G=r(),H=s("p"),H.innerHTML=ge,J=r(),L=s("ol"),L.innerHTML=we,K=r(),M=s("p"),M.textContent=be,V=r(),P=s("p"),P.innerHTML=xe,Z=r(),I=s("p"),I.textContent=Ce,$=r(),q=s("p"),q.innerHTML=ke,this.h()},l(e){h=a(e,"H1",{"data-svelte-h":!0}),o(h)!=="svelte-1ajilk8"&&(h.textContent=te),A=l(e),m=a(e,"P",{"data-svelte-h":!0}),o(m)!=="svelte-1aqlmww"&&(m.textContent=ne),j=l(e),d=a(e,"P",{"data-svelte-h":!0}),o(d)!=="svelte-15rfutm"&&(d.textContent=ie),S=l(e),u=a(e,"H2",{id:!0,"data-svelte-h":!0}),o(u)!=="svelte-1esky6d"&&(u.textContent=se),W=l(e),p=a(e,"UL",{"data-svelte-h":!0}),o(p)!=="svelte-1gkbmg1"&&(p.innerHTML=ae),z=l(e),f=a(e,"P",{"data-svelte-h":!0}),o(f)!=="svelte-k9bc9o"&&(f.innerHTML=oe),U=l(e),v=a(e,"P",{"data-svelte-h":!0}),o(v)!=="svelte-knqgqj"&&(v.textContent=re),N=l(e),y=a(e,"P",{"data-svelte-h":!0}),o(y)!=="svelte-1xi61vf"&&(y.textContent=le),E=l(e),c=a(e,"P",{"data-svelte-h":!0}),o(c)!=="svelte-1i666zw"&&(c.textContent=he),O=l(e),g=a(e,"P",{"data-svelte-h":!0}),o(g)!=="svelte-eytdda"&&(g.textContent=ue),B=l(e),w=a(e,"P",{"data-svelte-h":!0}),o(w)!=="svelte-hyieue"&&(w.textContent=me),D=l(e),b=a(e,"P",{"data-svelte-h":!0}),o(b)!=="svelte-1lcc1b0"&&(b.innerHTML=de),F=l(e),x=a(e,"P",{"data-svelte-h":!0}),o(x)!=="svelte-1ohqqo6"&&(x.textContent=pe),R=l(e),C=a(e,"P",{"data-svelte-h":!0}),o(C)!=="svelte-17t8hpc"&&(C.textContent=fe),Q=l(e),k=a(e,"P",{"data-svelte-h":!0}),o(k)!=="svelte-10to6in"&&(k.textContent=ve),X=l(e),T=a(e,"P",{"data-svelte-h":!0}),o(T)!=="svelte-1bvjgpu"&&(T.innerHTML=ye),Y=l(e),_=a(e,"OL",{"data-svelte-h":!0}),o(_)!=="svelte-cgxadu"&&(_.innerHTML=ce),G=l(e),H=a(e,"P",{"data-svelte-h":!0}),o(H)!=="svelte-whu0lt"&&(H.innerHTML=ge),J=l(e),L=a(e,"OL",{"data-svelte-h":!0}),o(L)!=="svelte-eyvyyj"&&(L.innerHTML=we),K=l(e),M=a(e,"P",{"data-svelte-h":!0}),o(M)!=="svelte-7auuw7"&&(M.textContent=be),V=l(e),P=a(e,"P",{"data-svelte-h":!0}),o(P)!=="svelte-1olohyw"&&(P.innerHTML=xe),Z=l(e),I=a(e,"P",{"data-svelte-h":!0}),o(I)!=="svelte-1m4qgd9"&&(I.textContent=Ce),$=l(e),q=a(e,"P",{"data-svelte-h":!0}),o(q)!=="svelte-dt87u7"&&(q.innerHTML=ke),this.h()},h(){He(u,"id","surprises-to-me-")},m(e,t){n(e,h,t),n(e,A,t),n(e,m,t),n(e,j,t),n(e,d,t),n(e,S,t),n(e,u,t),n(e,W,t),n(e,p,t),n(e,z,t),n(e,f,t),n(e,U,t),n(e,v,t),n(e,N,t),n(e,y,t),n(e,E,t),n(e,c,t),n(e,O,t),n(e,g,t),n(e,B,t),n(e,w,t),n(e,D,t),n(e,b,t),n(e,F,t),n(e,x,t),n(e,R,t),n(e,C,t),n(e,Q,t),n(e,k,t),n(e,X,t),n(e,T,t),n(e,Y,t),n(e,_,t),n(e,G,t),n(e,H,t),n(e,J,t),n(e,L,t),n(e,K,t),n(e,M,t),n(e,V,t),n(e,P,t),n(e,Z,t),n(e,I,t),n(e,$,t),n(e,q,t)},p:ee,i:ee,o:ee,d(e){e&&(i(h),i(A),i(m),i(j),i(d),i(S),i(u),i(W),i(p),i(z),i(f),i(U),i(v),i(N),i(y),i(E),i(c),i(O),i(g),i(B),i(w),i(D),i(b),i(F),i(x),i(R),i(C),i(Q),i(k),i(X),i(T),i(Y),i(_),i(G),i(H),i(J),i(L),i(K),i(M),i(V),i(P),i(Z),i(I),i($),i(q))}}}class Ae extends Le{constructor(h){super(),Me(this,h,null,Pe,_e,{})}}export{Ae as component};
---
+++ .vercel/output/static/_app/immutable/nodes/24.CQu-eu3D.js
@@ -0,0 +1,12 @@
+import{s as o}from"../chunks/scheduler.Cu-e96pa.js";import{S as n,i as r}from"../chunks/index.DvinRJN7.js";import"../chunks/marked.esm.CuVPQiC1.js";function a(t){return source=`
+    # how to write an email to a researcher (undergrad)
+
+1. Introduction
+2. How you found them
+3. Specific questions
+4. Background
+5. Intention/call to action
+
+Example:
+
+![Screenshot 2024-11-19 at 4.00.38 PM.png](static/Screenshot 2024-11-19 at 4.00.38 PM.png)`,[]}class u extends n{constructor(e){super(),r(this,e,a,null,o,{})}}export{u as component};
