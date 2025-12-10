7a93c97bad358af03c6686c680b613345f346820
Abinaya Dinesh <adinesh@berkeley.edu>
2025-12-10 18:42:19 -0500

    more style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,5 +22,7 @@
     font-size: 1.5em;
     padding-top: 0.2em;
     padding-bottom: 0.7em;
+    background-color: #000000;
+    background: #000000;
   }
 </style>
--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -2,24 +2,22 @@
     <h1>General</h1>
     <ul>
       <li>
-        <a href = "./writing/fav_pubs">01.07.25 favorite publications of 2024</a>
+        01.07.25 <a href = "./writing/fav_pubs">favorite publications of 2024</a>
       </li>
       <li>
-        <a href = " https://docs.google.com/document/d/1UOtDPD3F_469Qe_RGj4N10eKHTV5VavyjYZcgpg0Yyw/edit?usp=sharing">01.22.25 a lit review including parallelized trajectory optimization</a>
+        01.22.25 <a href = " https://docs.google.com/document/d/1UOtDPD3F_469Qe_RGj4N10eKHTV5VavyjYZcgpg0Yyw/edit?usp=sharing">a lit review including parallelized trajectory optimization</a>
       </li>
       <li>
-        <a href = "./writing/driving">01.07.25 how to drive to maximize your passengers comfort</a>
+        01.07.25 <a href = "./writing/driving">how to drive to maximize your passengers comfort</a>
       </li>
       <li>
-        <a href = "./writing/researcher">11.19.24 how to write an email to a researcher (undergrad)</a>
+        11.19.24 <a href = "./writing/researcher"> how to write an email to a researcher (undergrad)</a>
       </li>
       <li>
-        <a href = "./writing/homeforgenius">07.24.24 IAS: an environment for genius</a>
+        07.24.24 <a href = "./writing/homeforgenius"> IAS: an environment for genius</a>
       </li>
       <li>
-        <a href="./writing/readthenews"
-          >⭐️ 12.28.23 should tech people read the news?</a
-        >
+        ⭐️ 12.28.23 <a href="./writing/readthenews"> should tech people read the news?</a>
       </li>
     
     </ul>
@@ -27,43 +25,32 @@
     <h1>Ag</h1>
     <ul>
       <li>
-        <a href="./writing/economiesofscale"
-          >2.17.24 why do economies at scale break down with agriculture?</a
+        2.17.24 <a href="./writing/economiesofscale"> why do economies at scale break down with agriculture?</a
         >
       </li>
       <li>
-        <a
+        8.6.23 <a
           href="https://docs.google.com/document/d/1UwPVsQsHbcuBo2-CUYT1dk7Ymp6Jl2NDv5esT4iS5vw/edit?usp=sharing"
-          >8.6.23 the deep connections between soil and greenhouse gas emissions
+          > the deep connections between soil and greenhouse gas emissions
         </a>
       </li>
       <li>
-        <a
+        4.21.23 <a
           href="https://docs.google.com/document/d/1aw5Nn2ph2TABpfSMn1JqiJZblZ7yP6rOiH7Hd4tXpQY/edit?usp=sharing"
-          >4.21.23 the role of mycorrhizal fungi in plant growth and development
+          > the role of mycorrhizal fungi in plant growth and development
         </a>
       </li>
       <li>
-        <a href="./writing/spectrometer">07.23.23 deep dives on spectrometers</a>
+        07.23.23 <a href="./writing/spectrometer"> deep dives on spectrometers</a>
       </li>
       <li>
-        <a
-          href="https://docs.google.com/presentation/d/1Z8ebEN76z4dJMa5gXSBFIMoTtN-JtdcPauNKf1b8Y-g/edit?usp=sharing"
-          >5.2.23 metagenomic time series analysis on nitrate reduction pathway in
-          cattle gut</a
-        >
+        5.2.23 <a href="https://docs.google.com/presentation/d/1Z8ebEN76z4dJMa5gXSBFIMoTtN-JtdcPauNKf1b8Y-g/edit?usp=sharing"> metagenomic time series analysis on nitrate reduction pathway in cattle gut</a>
       </li>
       <li>
-        <a
-          href="https://www.notion.so/Winogradsky-Columns-Report-3-fb51de6e68e84730909f24d890c7c487?pvs=4"
-          >4.30.23 how synthetic fertilizer impacts microbial diversity (see the
-          visual beauty of microbes!)
-        </a>
+        4.30.23 <a href="https://www.notion.so/Winogradsky-Columns-Report-3-fb51de6e68e84730909f24d890c7c487?pvs=4"> how synthetic fertilizer impacts microbial diversity</a> (see the visual beauty of microbes!)
       </li>
       <li>
-        <a href="./writing/agonpublichealth"
-          >the impacts of conventional agriculture on public health</a
-        >
+        <a href="./writing/agonpublichealth">the impacts of conventional agriculture on public health</a>
       </li>
       <li>
         <a
